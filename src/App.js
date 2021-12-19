b8634687ccb2dafc43476f784169e9aab1ec1916
RayKilburg <rkilburg42@gmail.com>
2021-12-18 23:51:30 -0500

    useRefExample2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import UseRefExample from './components/UseRefExample';
 import Counter from './components/Counter';
+import UseRefExample2 from './components/UseRefExample2';
 
 function App() {
   return (
     <div className="App">
-      <UseRefExample />
-      <Counter />
+      <UseRefExample2 />
     </div>
   );
 }
