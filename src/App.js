4f5b8ce88efab87c81ca47c3bd1751cc17402836
RayKilburg <rkilburg42@gmail.com>
2021-12-19 09:07:02 -0500

    useref 3 starter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import UseRefExample from './components/UseRefExample';
 import Counter from './components/Counter';
 import UseRefExample2 from './components/UseRefExample2';
+import UseRefExample3 from './components/UseRefExample3';
 
 function App() {
   return (
     <div className="App">
-      <UseRefExample2 />
+      <UseRefExample3 />
     </div>
   );
 }
---
+++ src/components/UseRefExample3.jsx
@@ -0,0 +1,9 @@
+function UseRefExample3() {
+    return (
+        <div>
+           <h2>Example 3</h2>
+        </div>
+    )
+}
+
+export default UseRefExample3
