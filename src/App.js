0cd6c362c6b23674a5f99e8fa2da67466cce5028
RayKilburg <rkilburg42@gmail.com>
2021-12-20 11:43:34 -0500

    useMemo example

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import UseRefExample from './components/UseRefExample';
 import Counter from './components/Counter';
 import UseRefExample2 from './components/UseRefExample2';
 import UseRefExample3 from './components/UseRefExample3';
+import UseMemoExample from './components/UseMemoExample';
 
 function App() {
   return (
     <div className="App">
-      <UseRefExample3 />
+      <UseMemoExample />
     </div>
   );
 }
