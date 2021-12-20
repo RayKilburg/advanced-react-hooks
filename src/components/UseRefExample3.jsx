0fa386e2ec73bc027b2f91da6d27849c9f8b689e
RayKilburg <rkilburg42@gmail.com>
2021-12-19 23:43:44 -0500

    useRef example 3 memory leak error fix

--- src/components/UseRefExample3.jsx
+++ src/components/UseRefExample3.jsx
@@ -1,7 +1,14 @@
+import { useState } from "react"
+import Todo from "./Todo"
+
 function UseRefExample3() {
+
+    const [showTodo, setShowTodo] = useState(true)
+
     return (
         <div>
-           <h2>Example 3</h2>
+           {showTodo && <Todo />}
+           <button className="btn btn-primary" onClick={() => setShowTodo(!showTodo)}>Toggle Todo</button>
         </div>
     )
 }
