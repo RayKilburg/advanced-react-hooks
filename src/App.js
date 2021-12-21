b717100a0cde2554efc3e2ecd65c906ab04ab7cf
Ray <rkilburg42@gmail.com>
2021-12-21 00:55:20 -0500

    custom hook 2 - useLocalStorage

--- src/App.js
+++ src/App.js
@@ -5,11 +5,12 @@ import UseRefExample3 from './components/UseRefExample3';
 import UseMemoExample from './components/UseMemoExample';
 import UseCallbackExample from './components/UseCallbackExample';
 import CustomHookExample1 from './components/CustomHookExample1';
+import CustomHookExample2 from './components/CustomHookExample2';
 
 function App() {
   return (
-    <div className="App">
-      <CustomHookExample1 />
+    <div className="container mt-5">
+      <CustomHookExample2 />
     </div>
   );
 }
