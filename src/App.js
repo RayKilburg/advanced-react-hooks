a04c733525959fc3b24552eb8304c2330a70fd8e
Ray <rkilburg42@gmail.com>
2021-12-20 22:02:40 -0500

    custom hook 1 - useFetch

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import UseRefExample2 from './components/UseRefExample2';
 import UseRefExample3 from './components/UseRefExample3';
 import UseMemoExample from './components/UseMemoExample';
 import UseCallbackExample from './components/UseCallbackExample';
+import CustomHookExample1 from './components/CustomHookExample1';
 
 function App() {
   return (
     <div className="App">
-      <UseCallbackExample />
+      <CustomHookExample1 />
     </div>
   );
 }
