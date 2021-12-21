2cc9f80d0094281fff157bb80a390a6b498db81e
Ray <rkilburg42@gmail.com>
2021-12-20 19:22:31 -0500

    useCallback example

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import Counter from './components/Counter';
 import UseRefExample2 from './components/UseRefExample2';
 import UseRefExample3 from './components/UseRefExample3';
 import UseMemoExample from './components/UseMemoExample';
+import UseCallbackExample from './components/UseCallbackExample';
 
 function App() {
   return (
     <div className="App">
-      <UseMemoExample />
+      <UseCallbackExample />
     </div>
   );
 }
