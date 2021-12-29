a20d4650f86bd784447913c2e1b560aabc0be44c
RayKilburg <rkilburg42@gmail.com>
2021-12-28 23:22:44 -0500

    api call example axios finish

--- src/components/ApiCallExample.jsx
+++ src/components/ApiCallExample.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import axios from 'axios'
 
 function ApiCallExample() {
+    const [data, setData] = useState([]);
+
+    const fetchData = async () => {
+        const { data } = await axios.get("https://jsonplaceholder.typicode.com/posts/1/comments");
+        setData(data)
+    }
+
+    useEffect(() => {
+        fetchData()
+    }, [])
+
+    console.log(data)
+
     return (
         <div>
-            api example
+            <ul>
+                {data.map((item) => {
+                    return (
+                        <>
+                            <li key={item.id}>Id: {item.id}</li>
+                            <li key={item.name}>Name: {item.name}</li>
+                            <li key={item.email}>Email: {item.email}</li>
+                            <p key={item.body}><b>{item.body}</b></p>
+                            <hr />
+                        </>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
 
 export default ApiCallExample
+
